984386a5e764d342e57334352d6443992159a1a3
Kangrui Leng <53813482+Swida-Alba@users.noreply.github.com>
2023-06-11 11:38:23 +0800

    modified

---
+++ FindDirect.py
@@ -0,0 +1,24 @@
+import warnings
+import pandas as pd
+warnings.filterwarnings("ignore")
+from coana import FindNeuronConnection
+
+
+fc = FindNeuronConnection(
+    # please provide your own neuprint token, which can be found at https://neuprint.janelia.org/account
+    token='',
+    dataset = 'hemibrain:v1.2.1', 
+    sourceNeurons = ['KC.*'], # pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist()
+    targetNeurons = ['MBON.*'], # pd.read_excel('targetNeurons.xlsx', header=None).iloc[:,0].tolist()
+    # sourceNeurons = pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist(),
+    # targetNeurons = pd.read_excel('targetNeurons.xlsx', header=None).iloc[:,0].tolist(),
+    custom_source_name = '', # you can specify a custom name for the source neurons, especially when you are using a list of many types of neurons or a list of neurons read from a file
+    custom_target_name = '', # you can specify a custom name for the target neurons
+    min_synapse_num = 1,
+    min_traversal_probability = 0.001,
+    showfig = False,
+)
+
+fc.InitializeNeuronInfo()
+fc.FindDirectConnections(full_data=False)
+
