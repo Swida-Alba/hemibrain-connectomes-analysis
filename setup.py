755735729330664a890b6b2c44eb108bd3e197d8
Kangrui Leng <53813482+Swida-Alba@users.noreply.github.com>
2023-06-11 11:54:16 +0800

    modified

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import subprocess
 subprocess.check_call(['pip3', 'install', 'numpy'])
 subprocess.check_call(['pip3', 'install', 'openpyxl'])
 subprocess.check_call(['pip3', 'install', 'pandas==1.5.1']) ### don't use pandas >= 2.0.0
+subprocess.check_call(['pip3', 'install', 'plotly'])
 subprocess.check_call(['pip3', 'install', 'bokeh'])
 subprocess.check_call(['pip3', 'install', 'matplotlib'])
 subprocess.check_call(['pip3', 'install', 'seaborn'])
@@ -11,5 +12,4 @@ subprocess.check_call(['pip3', 'install', 'scipy'])
 subprocess.check_call(['pip3', 'install', 'neuprint-python'])
 subprocess.check_call(['pip3', 'install', 'navis'])
 subprocess.check_call(['pip3', 'install', 'networkx'])
-subprocess.check_call(['pip3', 'install', 'plotly'])
 subprocess.check_call(['pip3', 'install', 'img2pdf'])
