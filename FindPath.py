d10f20b9d87975b38f7120177f2f454a9e54260d
Kangrui Leng <53813482+Swida-Alba@users.noreply.github.com>
2023-06-11 11:42:18 +0800

    modified:

--- FindPath.py
+++ FindPath.py
@@ -8,13 +8,13 @@ fc = FindNeuronConnection(
     # please provide your own neuprint token, which can be found at https://neuprint.janelia.org/account
     token='',
     dataset = 'hemibrain:v1.2.1', 
-    sourceNeurons = ['.*_PN.*_R'], # pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist()
+    sourceNeurons = ['VA1d_.*PN.*_R'], # pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist()
     targetNeurons = ['MBON.*'], # pd.read_excel('targetNeurons.xlsx', header=None).iloc[:,0].tolist()
     # sourceNeurons = pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist(),
     # targetNeurons = pd.read_excel('targetNeurons.xlsx', header=None).iloc[:,0].tolist(),
     custom_source_name = '', # you can specify a custom name for the source neurons, especially when you are using a list of many types of neurons or a list of neurons read from a file
     custom_target_name = '', # you can specify a custom name for the target neurons
-    min_synapse_num = 1,
+    min_synapse_num = 10,
     min_traversal_probability = 1e-6,
     showfig = False,
     max_interlayer=1,
