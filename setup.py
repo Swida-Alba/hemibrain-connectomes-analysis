19f36476cbda56b7c664e38cfec19261c7f438a7
Kangrui Leng <53813482+Swida-Alba@users.noreply.github.com>
2023-06-11 21:21:11 +0800

    M

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import subprocess
 
 # Install packages
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'numpy'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'openpyxl'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'pandas==1.5.1']) ### don't use pandas >= 2.0.0
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'plotly'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'bokeh'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'matplotlib'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'seaborn'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'scipy'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'neuprint-python'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'navis'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'networkx'])
-subprocess.check_call(['python3.11', '-m', 'pip', 'install', 'img2pdf'])
+subprocess.check_call(['pip3', 'install', 'numpy'])
+subprocess.check_call(['pip3', 'install', 'openpyxl'])
+subprocess.check_call(['pip3', 'install', 'pandas==1.5.1']) ### don't use pandas >= 2.0.0
+subprocess.check_call(['pip3', 'install', 'plotly'])
+subprocess.check_call(['pip3', 'install', 'bokeh'])
+subprocess.check_call(['pip3', 'install', 'matplotlib'])
+subprocess.check_call(['pip3', 'install', 'seaborn'])
+subprocess.check_call(['pip3', 'install', 'scipy'])
+subprocess.check_call(['pip3', 'install', 'neuprint-python'])
+subprocess.check_call(['pip3', 'install', 'navis'])
+subprocess.check_call(['pip3', 'install', 'networkx'])
+subprocess.check_call(['pip3', 'install', 'img2pdf'])
