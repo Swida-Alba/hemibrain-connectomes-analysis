3cf84366752dd5b9819caaae5272825124e48f9d
Kangrui Leng <53813482+Swida-Alba@users.noreply.github.com>
2023-07-09 12:32:53 +0800

    M

--- plot3dSkeleton.py
+++ plot3dSkeleton.py
@@ -10,15 +10,16 @@ vs = VisualizeSkeleton(
     neuron_alpha = 0.2,
     saveas = None,
     min_synapse_num = 1,
-    synapse_size = 3,
+    synapse_size = 2,
     synapse_alpha = 0.6,
-    mesh_roi = ['LH(R)','AL(R)','EB','gL(R)'],
+    mesh_roi = ['LH(R)','AL(R)','EB'],
     skeleton_mode = 'tube',
     synapse_mode = 'scatter',
     legend_mode = 'merge',
     use_size_slider = True,
     show_fig = True,
+    brain_mesh = 'whole',
 )
 
 vs.plot_neurons()
-vs.export_video(fps=30,rotate_plane='xy',synapse_size=2,scale=2,)
+vs.export_video(fps=30)
