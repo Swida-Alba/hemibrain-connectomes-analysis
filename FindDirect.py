cbebc881d61f6f6f3a8b9b744cf0ba37cd49cbee
Kangrui Leng <53813482+Swida-Alba@users.noreply.github.com>
2024-08-01 14:28:45 +0800

    M

--- FindDirect.py
+++ FindDirect.py
@@ -4,11 +4,12 @@ warnings.filterwarnings("ignore")
 from coana import FindNeuronConnection
 
 fc = FindNeuronConnection(
-    # please provide your own neuprint token, which can be found at https://neuprint.janelia.org/account
+    # please provide your own neuprint token, which can be found at https://neuprint.janelia.org/
     token='',
+    save_folder='', # the folder to save the data
     dataset = 'hemibrain:v1.2.1', 
-    sourceNeurons = ['KC.*'], # pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist()
-    targetNeurons = ['MBON.*'], # pd.read_excel('targetNeurons.xlsx', header=None).iloc[:,0].tolist()
+    sourceNeurons = ['MBON.*'], # pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist()
+    targetNeurons = ['aMe.*'], # pd.read_excel('targetNeurons.xlsx', header=None).iloc[:,0].tolist()
     # sourceNeurons = pd.read_excel('sourceNeurons.xlsx', header=None).iloc[:,0].tolist(),
     # targetNeurons = pd.read_excel('targetNeurons.xlsx', header=None).iloc[:,0].tolist(),
     custom_source_name = '', # you can specify a custom name for the source neurons, especially when you are using a list of many types of neurons or a list of neurons read from a file
